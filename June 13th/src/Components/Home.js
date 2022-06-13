4c4ef2a64fb5f7c41e1a54e5fba9e057056539b0
astha <68704080+Astha-T@users.noreply.github.com>
2022-06-13 18:48:59 +0530

    Add files via upload

---
+++ June 13th/src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div class="">
+           <div class="">
+             <p>Shop By Categories</p>
+           </div>
+           <div class="">
+             <p>Our Products</p>
+           </div>
+        </div>
+    )
+}
+
+export default Home;
